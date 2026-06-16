0f2dec95fc69bd9a97b8fab5f5487e876bfa6f3f
neham21062005 <nehalakshmi5345@gmail.com>
2026-06-16 11:41:16 +0530

    Update API key retrieval method for genai

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from dotenv import load_dotenv
-import os
 import streamlit as st
 import google.generativeai as genai
 
 
-load_dotenv()
-
 genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
+    api_key=st.secrets("GEMINI_API_KEY")
 )
 
 model = genai.GenerativeModel("gemini-2.5-flash")
