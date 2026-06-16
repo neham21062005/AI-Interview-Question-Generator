8f7b33badfa3f53ff197ff2cac228d0f70fa39b3
neham21062005 <nehalakshmi5345@gmail.com>
2026-06-16 11:12:19 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+import google.generativeai as genai
+
+
+load_dotenv()
+
+genai.configure(
+    api_key=os.getenv("GEMINI_API_KEY")
+)
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+st.title("🚀 AI Interview Question Generator")
+
+role = st.text_input("Job Role")
+skills = st.text_input("Skills")
+
+difficulty = st.selectbox(
+    "Difficulty",
+    ["Beginner", "Intermediate", "Advanced"]
+)
+
+company = st.selectbox(
+    "Target Company",
+    ["Wipro", "TCS", "Infosys", "Accenture", "Google"]
+)
+
+if st.button("Generate Questions"):
+
+    prompt = f"""
+    Generate 5 {difficulty} interview questions
+    for a {role} applying to {company}
+    with skills {skills}.
+
+    Give:
+    1. Question
+    2. Sample Answer
+    3. Difficulty
+    4. Interview Tips
+    """
+
+    response = model.generate_content(prompt)
+
+    st.write(response.text)
+
+    st.download_button(
+        "Download Questions",
+        response.text,
+        file_name="interview_questions.txt"
+    )
---
+++ test.py
@@ -0,0 +1,7 @@
+import google.generativeai as genai
+
+load_dotenv()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+for m in genai.list_models():
+    print(m.name)
